0d9ebdf32e41926d80ef2c196df631819ac73b70
Mario <mmariob196@gmail.com>
2020-08-14 15:38:10 +0200

    added copy on clipboard

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,18 @@ function getPassword(){
         password += chars.substring(randomNumber, randomNumber+1);
     }
     document.getElementById("password").value = password;
+    alertBox.innerHTML = "New Password Copied: <br>" + password;
+}
+
+var alertBox = document.querySelector('.alertBox');
+
+function copyPassword(){
+    var copyPassText = document.getElementById("password");
+    copyPassText.select();
+    copyPassText.setSelectionRange(0,9999);
+    document.execCommand("copy");
+    alertBox.classList.toggle('active');
+    setTimeout(function(){
+        alertBox.classList.toggle('active');
+    }, 2000)
 }
